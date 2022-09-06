560e34e786663aa38cf0ff8f21326fcf0708df49
roozbeh <45459584+roozbehshafieebafti@users.noreply.github.com>
2022-09-06 18:14:26 +0430

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import five from 'johnny-five';
+
+const board = new five.Board();
+
+console.log('pins',board.pins)
+console.log('id',board.id)
+console.log('io',board.io)
+console.log('isReady',board.isReady)
+console.log('port',board.port)
+console.log('port',board.port)
