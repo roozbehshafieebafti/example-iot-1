1327ef78df0eae86647e3e63e2c9c2aeae51295f
roozbeh <45459584+roozbehshafieebafti@users.noreply.github.com>
2022-09-06 18:37:40 +0430

    update ready

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
 import five from 'johnny-five';
 
 const board = new five.Board();
-const btn = new five.Button(15);
-const led = new five.Led(14);
 
-console.log('pins',board.pins)
-console.log('id',board.id)
-console.log('io',board.io)
-console.log('isReady',board.isReady)
-console.log('port',board.port)
-console.log('port',board.port)
+board.on(('ready'),()=>{
+    
+    const btn = new five.Button(15);
+    const led = new five.Led(14);
 
-console.log('------')
+    console.log('pins',board.pins)
+    console.log('id',board.id)
+    console.log('io',board.io)
+    console.log('isReady',board.isReady)
+    console.log('port',board.port)
+    console.log('port',board.port)
+    
+    console.log('------')
+    
+    console.log('downValue',btn.downValue)
+    console.log('holdtime',btn.holdtime)
+    console.log('id',btn.id)
+    console.log('pin',btn.pin)
+    console.log('upValue',btn.upValue)
+    
+    console.log('------')
+    
+    console.log('id', led.id)
+    console.log('pin', led.pin)
+})
 
-console.log('downValue',btn.downValue)
-console.log('holdtime',btn.holdtime)
-console.log('id',btn.id)
-console.log('pin',btn.pin)
-console.log('upValue',btn.upValue)
-
-console.log('------')
-
-console.log('id', led.id)
-console.log('pin', led.pin)
