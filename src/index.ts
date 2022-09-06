010e6a3c449bed4c1a38dcf71c3e3d4af8a74a54
roozbeh <45459584+roozbehshafieebafti@users.noreply.github.com>
2022-09-06 18:31:14 +0430

    add new logs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import five from 'johnny-five';
+import { Board, Button, Led } from 'johnny-five';
 
-const board = new five.Board();
+const board = new Board();
+const btn = new Button(15);
+const led = new Led(14)
 
 console.log('pins',board.pins)
 console.log('id',board.id)
@@ -8,3 +10,16 @@ console.log('io',board.io)
 console.log('isReady',board.isReady)
 console.log('port',board.port)
 console.log('port',board.port)
+
+console.log('------')
+
+console.log('downValue',btn.downValue)
+console.log('holdtime',btn.holdtime)
+console.log('id',btn.id)
+console.log('pin',btn.pin)
+console.log('upValue',btn.upValue)
+
+console.log('------')
+
+console.log('id', led.id)
+console.log('pin', led.pin)
